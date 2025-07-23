7bd33c221a1ee324f8eb4a0503716332c95584d4
Prateek Bhardwaj <prateekb1912@gmail.com>
2025-07-24 04:24:17 +0530

    fix config

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from .schemas import ScoreSubmissionInput
 from .services import submit_score, get_top_players, get_user_rank
 from fastapi.middleware.cors import CORSMiddleware
 
-newrelic.agent.initialize("newrelic.ini")
+newrelic.agent.initialize("./app/newrelic.ini")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
