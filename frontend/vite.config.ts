47ebfc98991cdeef47a7ff42d9b0246a3af87933
Prateek Bhardwaj <prateekb1912@gmail.com>
2025-07-24 03:44:29 +0530

    use static url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(({}) => {
   return {
     plugins: [react()],
     server: {
       proxy: {
-        "/api": env.LEADERBOARD_API_URL || "http://localhost:8000",
+        "/api": "https://gaming-leaderboard-9euy.onrender.com",
       },
     },
   };
